56dc112046aa7a657458f349769f3ad73483c853
Eric Daniel Fournier <me@ericdfournier.com>
2023-04-06 12:02:31 -0700

    Update CARB priority population download script

--- data/carb_priority_populations/download.sh
+++ data/carb_priority_populations/download.sh
@@ -1,11 +1,11 @@
 # Set working directory
 
-chdir /Users/edf/repos/carb_elec/data/
+chdir /Users/edf/repos/carb_elec/data/carb_priority_populations/
 
-# CARB Priority Population Geodatabase
+# Download CARB Priority Population Geodatabase
 
 url='https://ww2.arb.ca.gov/sites/default/files/auction-proceeds/map/PriorityPopulations2022CES4.gdb.zip';
 curl -o carb_priority_populations.gdb.zip $url;
-mkdir ./carb_priority_populations;
-unzip carb_priority_populations.gdb.zip -d ./carb_priority_populations;
+mkdir raw;
+unzip carb_priority_populations.gdb.zip -d ./raw;
 rm carb_priority_populations.gdb.zip;
