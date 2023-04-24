b4e4955b997044d0d4e42dfd4acfab826b8ba6c8
Eric Daniel Fournier <me@ericdfournier.com>
2023-04-24 13:09:27 -0700

    Add CARB Air Basins and Districts import workflow

---
+++ data/carb_boundaries/README.md
@@ -0,0 +1,4 @@
+# README.md
+
+## Data Source
+https://data.ca.gov/dataset/california-air-resources-board-gis-datasets
---
+++ data/carb_boundaries/import.sh
@@ -0,0 +1,53 @@
+#!/usr/bin/env bash
+set -ue  # Set nounset and errexit Bash shell attributes.
+
+################################################################################
+# Import CARB Air Quality Management Boundaries
+#
+# Creates tables:
+# - carb.ca_air_basins
+# - carb.ca_air_districts
+#
+# Converts SRS to EPSG:3310 (NAD83 / California Albers).
+################################################################################
+
+format='PostgreSQL'
+dst="postgresql://$PGUSER@$PGHOST/carb"
+schema='carb'
+src='/Users/edf/repos/carb_elec/data/carb_boundaries/raw/'
+out='/Users/edf/repos/carb_elec/data/carb_boundaries/'
+
+file='CaAirBasin.shp'
+table='ca_air_basins'
+
+ogr2ogr -f $format $dst \
+    $src$file \
+    -lco SCHEMA=$schema \
+    -nln $table  \
+    -nlt MULTISURFACE \
+    -t_srs EPSG:3310 \
+    -lco GEOMETRY_NAME=geom \
+    -unsetFieldWidth \
+    -emptyStrAsNull \
+    -lco DESCRIPTION=$table \
+    --debug ON
+
+ogrinfo -so -ro $dst $schema.$table > $out$table'_orginfo.txt'
+
+
+file='CaAirDistrict.shp'
+table='ca_air_districts'
+
+ogr2ogr -f $format $dst \
+    $src$file \
+    -lco SCHEMA=$schema \
+    -nln $table  \
+    -nlt MULTISURFACE \
+    -t_srs EPSG:3310 \
+    -lco GEOMETRY_NAME=geom \
+    -unsetFieldWidth \
+    -emptyStrAsNull \
+    -lco DESCRIPTION=$table \
+    --debug ON
+
+ogrinfo -so -ro $dst $schema.$table > $out$table'_orginfo.txt'
