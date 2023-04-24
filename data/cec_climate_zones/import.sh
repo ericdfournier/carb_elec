7fdf91c286132d6a7876f20441e595db2a2f57f6
Eric Daniel Fournier <me@ericdfournier.com>
2023-04-24 15:25:19 -0700

    Finalize cec climate zone import workflow

--- data/cec_climate_zones/import.sh
+++ data/cec_climate_zones/import.sh
@@ -10,11 +10,14 @@ set -ue  # Set nounset and errexit Bash shell attributes.
 # Converts SRS to EPSG:3310 (NAD83 / California Albers).
 ################################################################################
 
+# Set environment parameters
 format='PostgreSQL'
 dst="postgresql://$PGUSER@$PGHOST/carb"
 schema='cec'
 src='/Users/edf/repos/carb_elec/data/cec_climate_zones/raw/'
 out='/Users/edf/repos/carb_elec/data/cec_climate_zones/'
+
+# Import climate zone table
 file='ca_building_climate_zones.geojson'
 table='ca_building_climate_zones_2021'
 
@@ -29,4 +32,6 @@ ogr2ogr -f $format $dst \
     -lco DESCRIPTION=$table \
     --debug ON
 
+# Write table metadata
+table='ca_building_climate_zones_2021'
 ogrinfo -so -ro $dst $schema.$table > $out$table'_orginfo.txt'
