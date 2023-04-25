5e9602007438a12a463ca48f1adba8775690e8d9
Eric Daniel Fournier <me@ericdfournier.com>
2023-04-25 14:27:29 -0700

    Add main orchestration shell script

---
+++ main.sh
@@ -0,0 +1,66 @@
+#!/usr/bin/env bash
+set -ue  # Set nounset and errexit Bash shell attributes.
+
+# Set working directory
+dir=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
+cd $dir
+
+################################################################################
+# Orchestrate Import Routines
+################################################################################
+
+# Import Census American Community Survey
+echo "Importing Census American Community Survey Data"
+./census_american_community_survey/import.sh
+
+# Import CARB Priority Populations Data
+echo "Importing CARB Priority Populations Data..."
+./carb_priority_populations/import.sh
+
+# Import CARB Boundaries Data
+echo "Importing CARB Boundaries Data..."
+./carb_boundaries/import.sh
+
+# Import CDPH Climate Hazard and Vulnerability Index Data
+echo "Importing CDPH Hazard Vulnerability Index Data..."
+./cdph_climate_hazard_vulnerability_index/import.sh
+
+# Import CEC Building Climate Zone Data
+echo "Importing CEC Climate Zone Data..."
+./cec_climate_zones/import.sh
+
+# Import Community Choice Aggregations Data
+echo "Importing Community Choice Aggregations Data..."
+./community_choice_aggregations/import.sh
+
+# Import CPUC Utility Affordability Ratio Data
+echo "Importing CPUC Utility Affordability Ratio Data..."
+./cpuc_utility_affordability_ratios/import.sh
+
+# Import DOE Lead Tool Data
+echo "Importing DOE Lead Tool Data..."
+./cpuc_utility_affordability_ratios/import.sh
+
+# Import OEHHA CalEnviroScreen-4.0 Data
+echo "Importing OEHHA CalEnviroScreen-4.0 Data..."
+./oehha_cal_enviro_screen_4/import.sh
+
+# Import Regional Energy Network Data
+echo "Importing Regional Energy Network Data"
+./regional_energy_networks/import.sh
+
+# Import PGE Grid Data
+echo "Importing PGE Grid Data..."
+./pge_grid/import.sh
+
+# Import SCE Grid Data
+echo "Importing SCE Grid Data..."
+./sce_grid/import.sh
+
+# Import SDGE Grid Data
+echo "Importing SDGE Grid Data..."
+./sdge_grid/import.sh
+
+# Import USEPA eGRID Data
+echo "Importing USEPA eGRID Data..."
+./usepa_egrid/import.sh
