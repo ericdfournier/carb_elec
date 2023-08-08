1e1af1a0208840ac958af89611acad5fdf2ad593
Eric Daniel Fournier <me@ericdfournier.com>
2023-08-08 15:00:27 -0700

    Continue NN refactoring

--- model/neural_network_classifier.py
+++ model/neural_network_classifier.py
@@ -105,6 +105,222 @@ def ImportRaw(sector):
 
     return raw
 
+#%% Compute ROC Statistics
+
+def ROCStats(y_test_binary, predict_test_score, n_classes):
+
+    fpr, tpr, roc_auc = dict(), dict(), dict()
+    fpr["micro"], tpr["micro"], _ = roc_curve(y_test_binary.ravel(), predict_test_score.ravel())
+    roc_auc["micro"] = auc(fpr["micro"], tpr["micro"])
+
+    for i in range(n_classes):
+        fpr[i], tpr[i], _ = roc_curve(y_test_binary[:, i], predict_test_score[:, i])
+        roc_auc[i] = auc(fpr[i], tpr[i])
+
+    fpr_grid = np.linspace(0.0, 1.0, 1000)
+
+    # Interpolate all ROC curves at these points
+    mean_tpr = np.zeros_like(fpr_grid)
+
+    for i in range(n_classes):
+        mean_tpr += np.interp(fpr_grid, fpr[i], tpr[i])  # linear interpolation
+
+    # Average it and compute AUC
+    mean_tpr /= n_classes
+
+    fpr["macro"] = fpr_grid
+    tpr["macro"] = mean_tpr
+    roc_auc["macro"] = auc(fpr["macro"], tpr["macro"])
+
+    return tpr, fpr, roc_auc
+
+#%% Compute Precision and Recall Statistics
+
+def PrecisionRecallStats(y_test_binary, predict_test_score, n_classes):
+
+    precision = dict()
+    recall = dict()
+    average_precision = dict()
+
+    for i in range(n_classes):
+        precision[i], recall[i], _ = precision_recall_curve(y_test_binary[:, i], predict_test_score[:, i])
+        average_precision[i] = average_precision_score(y_test_binary[:, i], predict_test_score[:, i])
+
+    precision["micro"], recall["micro"], _ = precision_recall_curve(
+        y_test_binary.ravel(), predict_test_score.ravel()
+    )
+    average_precision["micro"] = average_precision_score(y_test_binary, predict_test_score, average="micro")
+
+    return precision, recall, average_precision
+
+#%% Plot Micro and Macro ROC Curves
+
+def PlotROCAverages(tpr, fpr, roc_auc):
+
+    fig, ax = plt.subplots(figsize=(9, 9))
+
+    x_lin = np.linspace(0,1,100)
+    y_lin = np.linspace(0,1,100)
+
+    ax.plot(x_lin, y_lin, linestyle = '--', color = 'k', linewidth = 1.5)
+
+    ax.plot(
+        fpr["micro"],
+        tpr["micro"],
+        label=f"micro-average ROC curve (AUC = {roc_auc['micro']:.2f})",
+        color="deeppink",
+        linestyle=":",
+        linewidth=4,
+    )
+
+    ax.plot(
+        fpr["macro"],
+        tpr["macro"],
+        label=f"macro-average ROC curve (AUC = {roc_auc['macro']:.2f})",
+        color="navy",
+        linestyle=":",
+        linewidth=4,
+    )
+
+    plt.grid(True)
+    plt.axis("square")
+    plt.xlabel("False Positive Rate")
+    plt.ylabel("True Positive Rate")
+    plt.title("Extension of Receiver Operating Characteristic\nto Micro & Macro multiclass")
+    plt.legend()
+    plt.show()
+
+    return fig, ax
+
+#%% Plot One-vs-Rest ROC Curves
+
+def PlotROCOneVsRest(y_test_binary, predict_test_score, class_names, n_classes):
+
+    fig, ax = plt.subplots(figsize=(9, 9))
+
+    colors = sns.color_palette(None, n_classes)
+
+    for class_id, color in zip(range(n_classes), colors):
+        RocCurveDisplay.from_predictions(
+            y_test_binary[:, class_id],
+            predict_test_score[:, class_id],
+            name = f"ROC curve for {class_names[class_id]}",
+            color = color,
+            ax = ax,
+            plot_chance_level = (class_id == 0),
+        )
+
+    plt.grid(True)
+    plt.axis("square")
+    plt.xlabel("False Positive Rate")
+    plt.ylabel("True Positive Rate")
+    plt.title("Extension of Receiver Operating Characteristic\nto One-vs-Rest multiclass")
+    plt.legend()
+    plt.show()
+
+    return fig, ax
+
+#%% Plot Micro-Average Precision-Recall Curve
+
+def PlotPRMicroAverages(recall, precision, average_precision, y_test):
+
+    fig, ax = plt.subplots(1, 1, figsize=(9, 9))
+
+    display = PrecisionRecallDisplay(
+        recall=recall["micro"],
+        precision=precision["micro"],
+        average_precision=average_precision["micro"],
+        prevalence_pos_label=Counter(y_test.ravel())[1] / y_test.size,
+    )
+
+    display.plot(plot_chance_level = True, ax = ax)
+    ax.set_title("Precision-Recall Curve\n Micro-averaged Over All Classes")
+    ax.legend(loc = 'best')
+    ax.grid(True)
+
+    return fig, ax
+
+#%% Plot Class Level Precision-Recall Curves
+
+def PlotPRClassLevel(recall, precision, average_precision, n_classes):
+
+    fig, ax = plt.subplots(figsize=(9, 9))
+
+    f_scores = np.linspace(0.2, 0.8, num=4)
+    lines, labels = [], []
+    for f_score in f_scores:
+        x = np.linspace(0.01, 1)
+        y = f_score * x / (2 * x - f_score)
+        (l,) = plt.plot(x[y >= 0], y[y >= 0], color="gray", alpha=0.2)
+        plt.annotate("f1={0:0.1f}".format(f_score), xy=(0.9, y[45] + 0.02))
+
+    display = PrecisionRecallDisplay(
+        recall=recall["micro"],
+        precision=precision["micro"],
+        average_precision=average_precision["micro"],
+    )
+    display.plot(ax=ax, name="Micro-average precision-recall", color="gold")
+
+    for i, color in zip(range(n_classes), colors):
+        display = PrecisionRecallDisplay(
+            recall=recall[i],
+            precision=precision[i],
+            average_precision=average_precision[i],
+        )
+        display.plot(ax=ax, name=f"Precision-recall for class {i}", color=color)
+
+    handles, labels = display.ax_.get_legend_handles_labels()
+    handles.extend([l])
+    labels.extend(["iso-f1 curves"])
+    ax.set_xlim([0.0, 1.0])
+    ax.set_ylim([0.0, 1.05])
+    ax.legend(handles=handles, labels=labels, bbox_to_anchor=(1.05, 0.8))
+    ax.set_title("Precision-Recall Curve\n for Each Individual Class")
+
+    plt.show()
+
+    return fig, ax
+
+#%% Plot Within-Class Count Normalized Confusion Matrix
+
+def PlotWithinClassNormalizedConfusionMatrix(mlp_clf, X_test, y_test, class_names):
+
+    fig, ax = plt.subplots(1,1,figsize=(15,11))
+
+    cat_norm = ConfusionMatrixDisplay.from_estimator(
+        mlp_clf,
+        X_test,
+        y_test,
+        display_labels=class_names,
+        cmap=plt.cm.Blues,
+        normalize= 'true',
+        ax = ax
+    )
+
+    cat_norm.ax_.set_title('Confusion Matrix\nWithin-Class, Count Normalized')
+
+    return fig, ax
+
+#%% Plot Across Class Count Normalized Confusion Matrix
+
+def PlotAcrossClassCountNormalizedConfusionnMatrix(mlp_clf, X_test, y_test, class_names):
+
+    fig, ax = plt.subplots(1,1,figsize=(15,11))
+
+    all_norm = ConfusionMatrixDisplay.from_estimator(
+        mlp_clf,
+        X_test,
+        y_test,
+        display_labels=class_names,
+        cmap=plt.cm.Reds,
+        normalize= 'all',
+        ax = ax
+    )
+
+    all_norm.ax_.set_title('Confusion Matrix\nAll-Class, Count Normalized')
+
+    return fig, ax
+
 #%% Extract Training Data
 
 sector = 'single_family'
@@ -272,234 +488,33 @@ print(accuracy)
 print(confusion_mat)
 print(clf_report)
 
-#%% Generate Class Names
+#%% Generate Class Names and Counts
 
 class_names = list(np.sort(outputs[output_attrib[0]].unique()))[:-1]
+n_classes = len(np.unique(y_train))
 
-#%% Plot Within-Class Count Normalized Confusion Matrix
-
-fig, ax = plt.subplots(1,1,figsize=(15,11))
-
-cat_norm = ConfusionMatrixDisplay.from_estimator(
-    mlp_clf,
-    X_test,
-    y_test,
-    display_labels=class_names,
-    cmap=plt.cm.Blues,
-    normalize= 'true',
-    ax = ax
-)
-
-cat_norm.ax_.set_title('Confusion Matrix\nWithin-Class, Count Normalized')
-
-#%% Plot Full Sample Count Normalized Confusion Matrix
-
-fig, ax = plt.subplots(1,1,figsize=(15,11))
-
-all_norm = ConfusionMatrixDisplay.from_estimator(
-    mlp_clf,
-    X_test,
-    y_test,
-    display_labels=class_names,
-    cmap=plt.cm.Reds,
-    normalize= 'all',
-    ax = ax
-)
-
-all_norm.ax_.set_title('Confusion Matrix\nAll-Class, Count Normalized')
-
-plt.show()
-
-#%% Plot ROC Curve
+#%% Generate Binary Label Representation for Test Data
 
 y_test_binary = LabelBinarizer().fit(y_test).transform(y_test)
 
-#%% Calculate Macro ROC
+#%% Compute ROC and PR Stats
 
-macro_roc_auc_ovr = roc_auc_score(
-    y_test_binary,
-    predict_test_score,
-    multi_class="ovr",
-    average="macro",
-)
+tpr, fpr, roc_auc = ROCStats(
+    y_test_binary, predict_test_score, n_classes)
+precision, recall, average_precision = PrecisionRecallStats(
+    y_test_binary, predict_test_score, n_classes)
 
-#%% Compute ROC Statistics
+#%% Generate Diagnostic Plots
 
-def ROCStats(y_test_binary, predict_test_score, n_classes):
-
-    fpr, tpr, roc_auc = dict(), dict(), dict()
-    fpr["micro"], tpr["micro"], _ = roc_curve(y_test_binary.ravel(), predict_test_score.ravel())
-    roc_auc["micro"] = auc(fpr["micro"], tpr["micro"])
-
-    for i in range(n_classes):
-        fpr[i], tpr[i], _ = roc_curve(y_test_binary[:, i], predict_test_score[:, i])
-        roc_auc[i] = auc(fpr[i], tpr[i])
-
-    fpr_grid = np.linspace(0.0, 1.0, 1000)
-
-    # Interpolate all ROC curves at these points
-    mean_tpr = np.zeros_like(fpr_grid)
-
-    for i in range(n_classes):
-        mean_tpr += np.interp(fpr_grid, fpr[i], tpr[i])  # linear interpolation
-
-    # Average it and compute AUC
-    mean_tpr /= n_classes
-
-    fpr["macro"] = fpr_grid
-    tpr["macro"] = mean_tpr
-    roc_auc["macro"] = auc(fpr["macro"], tpr["macro"])
-
-    return tpr, fpr, roc_auc
-
-#%% Compute Precision and Recall Statistics
-
-def PrecisionRecallStats(y_test_binary, predict_test_score, n_classes):
-
-    precision = dict()
-    recall = dict()
-    average_precision = dict()
-
-    for i in range(n_classes):
-        precision[i], recall[i], _ = precision_recall_curve(y_test_binary[:, i], predict_test_score[:, i])
-        average_precision[i] = average_precision_score(y_test_binary[:, i], predict_test_score[:, i])
-
-    precision["micro"], recall["micro"], _ = precision_recall_curve(
-        y_test_binary.ravel(), predict_test_score.ravel()
-    )
-    average_precision["micro"] = average_precision_score(y_test_binary, predict_test_score, average="micro")
-
-    return precision, recall, average_precision
-
-#%% Plot Micro and Macro ROC Curves
-
-def PlotROCAverages(tpr, fpr, roc_auc):
-
-    fig, ax = plt.subplots(figsize=(9, 9))
-
-    x_lin = np.linspace(0,1,100)
-    y_lin = np.linspace(0,1,100)
-
-    ax.plot(x_lin, y_lin, linestyle = '--', color = 'k', linewidth = 1.5)
-
-    ax.plot(
-        fpr["micro"],
-        tpr["micro"],
-        label=f"micro-average ROC curve (AUC = {roc_auc['micro']:.2f})",
-        color="deeppink",
-        linestyle=":",
-        linewidth=4,
-    )
-
-    ax.plot(
-        fpr["macro"],
-        tpr["macro"],
-        label=f"macro-average ROC curve (AUC = {roc_auc['macro']:.2f})",
-        color="navy",
-        linestyle=":",
-        linewidth=4,
-    )
-
-    plt.grid(True)
-    plt.axis("square")
-    plt.xlabel("False Positive Rate")
-    plt.ylabel("True Positive Rate")
-    plt.title("Extension of Receiver Operating Characteristic\nto Micro & Macro multiclass")
-    plt.legend()
-    plt.show()
-
-    return fig, ax
-
-#%% Plot One-vs-Rest ROC Curves
-
-def PlotROCOneVsRest(y_test_binary, predict_test_score, class_names, n_classes):
-
-    fig, ax = plt.subplots(figsize=(9, 9))
-
-    colors = sns.color_palette(None, n_classes)
-
-    for class_id, color in zip(range(n_classes), colors):
-        RocCurveDisplay.from_predictions(
-            y_test_binary[:, class_id],
-            predict_test_score[:, class_id],
-            name = f"ROC curve for {class_names[class_id]}",
-            color = color,
-            ax = ax,
-            plot_chance_level = (class_id == 0),
-        )
-
-    plt.grid(True)
-    plt.axis("square")
-    plt.xlabel("False Positive Rate")
-    plt.ylabel("True Positive Rate")
-    plt.title("Extension of Receiver Operating Characteristic\nto One-vs-Rest multiclass")
-    plt.legend()
-    plt.show()
-
-    return fig, ax
-
-#%% Compute ROC Stats and Generate Diagnostic Plots
-
-n_classes = len(np.unique(y_train))
-
-tpr, fpr, roc_auc = ROCStats(y_test_binary, predict_test_score, n_classes)
-
-fig1, ax1 = PlotROCAverages(tpr, fpr, roc_auc)
-fig2, ax2 = PlotROCOneVsRest(y_test_binary, predict_test_score, class_names, n_classes)
-
-#%% Compute Precision and Recall Stats and Generate Diagnostic Plots
-
-precision, recall, average_precision = PrecisionRecallStats(y_test_binary, predict_test_score, n_classes)
-
-#%% Plot Micro-Average Precision-Recall Curve
-
-# TODO: Functionalize remaining plot routines below...
-
-display = PrecisionRecallDisplay(
-    recall=recall["micro"],
-    precision=precision["micro"],
-    average_precision=average_precision["micro"],
-    prevalence_pos_label=Counter(y_test.ravel())[1] / y_test.size,
-)
-
-display.plot(plot_chance_level = True)
-_ = display.ax_.set_title("Precision-Recall Curve\n Micro-averaged Over All Classes")
-_ = display.ax_.legend(loc = 'best')
-_ = display.ax_.grid(True)
-
-#%% Plot Class Level Precision-Recall Curves
-
-fig, ax = plt.subplots(figsize=(9, 9))
-
-f_scores = np.linspace(0.2, 0.8, num=4)
-lines, labels = [], []
-for f_score in f_scores:
-    x = np.linspace(0.01, 1)
-    y = f_score * x / (2 * x - f_score)
-    (l,) = plt.plot(x[y >= 0], y[y >= 0], color="gray", alpha=0.2)
-    plt.annotate("f1={0:0.1f}".format(f_score), xy=(0.9, y[45] + 0.02))
-
-display = PrecisionRecallDisplay(
-    recall=recall["micro"],
-    precision=precision["micro"],
-    average_precision=average_precision["micro"],
-)
-display.plot(ax=ax, name="Micro-average precision-recall", color="gold")
-
-for i, color in zip(range(n_classes), colors):
-    display = PrecisionRecallDisplay(
-        recall=recall[i],
-        precision=precision[i],
-        average_precision=average_precision[i],
-    )
-    display.plot(ax=ax, name=f"Precision-recall for class {i}", color=color)
-
-handles, labels = display.ax_.get_legend_handles_labels()
-handles.extend([l])
-labels.extend(["iso-f1 curves"])
-ax.set_xlim([0.0, 1.0])
-ax.set_ylim([0.0, 1.05])
-ax.legend(handles=handles, labels=labels, bbox_to_anchor=(1.05, 0.8))
-ax.set_title("Precision-Recall Curve\n for Each Individual Class")
-
-plt.show()
+fig1, ax1 = PlotROCAverages(
+    tpr, fpr, roc_auc)
+fig2, ax2 = PlotROCOneVsRest(
+    y_test_binary, predict_test_score, class_names, n_classes)
+fig3, ax3 = PlotPRMicroAverages(
+    recall, precision, average_precision, y_test)
+fig4, ax4 = PlotPRClassLevel(
+    recall, precision, average_precision, n_classes)
+fig5, ax5 = PlotWithinClassNormalizedConfusionMatrix(
+    mlp_clf, X_test, y_test, class_names)
+fig6, ax6 = PlotAcrossClassCountNormalizedConfusionMatrix(
+    mlp_clf, X_test, y_test, class_names)
